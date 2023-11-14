f3a0638065d1b924671c5c8ecf9e6f3731af9148
Tatum Baldwin <tjbaldwin@students.nic.edu>
2023-11-14 15:38:05 -0800

    pseudocode

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
 // Nothing here yet...
 // Make sure to link your JS in index.html!
+
+//  create car class
+
+// contruct car
+
+    //make
+
+    //model
+
+    //year
+
+// return all object information to console
+
+// create second car
+
+// create third car
+
+// create fourth car
+
+// push all cars to an array called allCars
+
+//create a function that returns the cars
+
+//create an array called allCars
+
+// Loop through allCars
+
+    // describe car 1
+
+    //describe car 2
+
+    //describe car 3 
+
+    // describe car 4
+
+// 
